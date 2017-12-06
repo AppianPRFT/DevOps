e2feb76ac0aa772d010d20345cd00782a81baf9e
samBruns <samuel.bruns@perficient.com>
2017-12-06 08:03:14 -0600

    Bulk update
    COMMIT 20

--- Deployment/README.md
+++
@@ -1,6 +0,0 @@
-# DevOps
-
-   - Master file containing application data and database file
-   
-          :SLA Managment System
-              - Reporting system
--- Versioning@tmp/durable-591437b3/jenkins-wrap.bat
+++
@@ -1,3 +0,0 @@
-@echo off 
-cmd /c ""C:\Program Files (x86)\Jenkins\workspace\DevOps-Deployment-Pipeline\Versioning@tmp\durable-591437b3\jenkins-main.bat"" > "C:\Program Files (x86)\Jenkins\workspace\DevOps-Deployment-Pipeline\Versioning@tmp\durable-591437b3\jenkins-log.txt" 2>&1
-echo %ERRORLEVEL% > "C:\Program Files (x86)\Jenkins\workspace\DevOps-Deployment-Pipeline\Versioning@tmp\durable-591437b3\jenkins-result.txt"
